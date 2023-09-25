31680f877cce511d25d305349c711b7a2c07dec9
dliendo <dliendo@somoswoxi.com>
2023-09-24 21:36:01 -0300

    ex into folders repository fix

--- src/components/Total.jsx
+++
@@ -1,8 +0,0 @@
-const Total = ({parts}) => {
-  const total = parts[0].exercises + parts[1].exercises + parts[2].exercises
-  return (
-    <div>Number of exercises {total}</div>
-  )
-}
-
-export default Total
